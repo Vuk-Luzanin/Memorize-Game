eccaf66f55798ef60f93c06a6e6a77c71ea1cfa6
Vuk Luzanin <luzaninvuk@gmail.com>
2025-02-12 23:57:21 +0100

    made code look clean

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -2,19 +2,19 @@
 
 // jer je deo UI
 // gatekeeper izmedju modela i UI -> ViewModel
+// $0 is first argument
+// promenljive klase se ne inicijalizuju redom -> pa ne smemo jednu promenljivu inicijalizovati drugom
+// static ispred imena je da emojis bude globalna promenljiva, ali u  namespaceu nase klase -> time ce se globane promenljive prve inicijalizovati, pa njima mozemo inicijalizovati ostale promenljive
 import SwiftUI
 
-func createCardContent(forPairAtIndex index: Int) -> String {
-    return ["🤗", "🫠", "😹", "👀", "🗣️", "👨‍🚀", "💅"][index]
-}
 
 class EmojiMemoryGame {
-    private var model = MemoryGame(
-        numberOfPairsOfCards: 4,
-        cardContentFactory: { (index: Int) -> String in
-            return ["🤗", "🫠", "😹", "👀", "🗣️", "👨‍🚀", "💅"][index]
-        }
-    )
+    private static let emojis = ["🤗", "🫠", "😹", "👀", "🗣️", "👨‍🚀", "💅"]
+    
+    private var model = MemoryGame(numberOfPairsOfCards: 4) { pairIndex in
+        return EmojiMemoryGame.emojis[pairIndex]
+    }
+    
     
     var cards: Array<MemoryGame<String>.Card> {
         return model.cards
